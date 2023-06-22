91aebc45ad57114114cbe9bbffb0d4b331777d2c
WiZ <ybenkhalifa@vidasoftware.it>
2023-06-22 11:52:37 +0200

    Add project files.

---
+++ Model/Error/ValidationError.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorValidation.Model
+{
+    public class Validation : IValidation
+    {
+        private bool _isValid;
+        
+        private IEnumerable<ValidationCheck> _checks;
+        public IEnumerable<ValidationError> GetValidationChecks()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<ValidationError> GetValidationErrors()
+        {
+            
+        }
+
+        public IEnumerable<ValidationError> GetWarnings()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsValidated() => _isValid;
+
+        public bool Validate()
+        {
+           _isValid = true;
+            foreach (ValidationCheck check in _checks) { 
+                bool checkValidated = check.Validate();
+
+                if (_isValid) _isValid = checkValidated;
+            }
+
+            return _isValid;
+        }
+    }
+
+    public class ValidationResult { 
+        public bool IsValid { get; set; }
+        public string Message { get; set; }
+    }
+    public class ValidationCheck : IValidationCheck
+    {
+        public IEnumerable<ValidationError> GetValidationErrors()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool IsValidated()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Validate()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class ValidationError
+    {
+        private string Description { get; set; }
+        private string Message { get; set; }
+    }
+}
---
+++ Model/IValidation.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ErrorValidation.Model
+{
+    public interface IValidation
+    {
+        public bool Validate();
+        public bool IsValidated();
+        public IEnumerable<ValidationError> GetValidationErrors();
+        public IEnumerable<ValidationError> GetWarnings();
+        public IEnumerable<ValidationError> GetValidationChecks();
+        
+
+    }
+    public interface IValidationCheck { 
+        public bool Validate();
+        public bool IsValidated();
+        public IEnumerable<ValidationError> GetValidationErrors();
+    }
+}
